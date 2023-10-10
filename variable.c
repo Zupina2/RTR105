9cb914ae1462d34925484d82009cbb6adc0f0950
Zupina2 <144107099+Zupina2@users.noreply.github.com>
2023-10-10 11:25:55 +0300

    Add files via upload

--- variable.c
+++ variable.c
@@ -37,7 +37,40 @@ printf("birth_year Mainīga vertiba uzreiz pec piešķiršanas (dec): %d\n",birt
 printf("birth_year Mainīga vertiba uzreiz pec piešķiršanas (hex): %x\n",birth_year);
 printf("birth_year Mainīga vertiba uzreiz pec piešķiršanas (oct): %o\n",birth_year);
 
+int i1, i2=0, i3 = 0x13, i4 = 025;
 
+int modified_birth_year = birth_year >> 1;
+printf("\n");
+printf("modified_birth_year Mainīga vertiba uzreiz pec piešķiršanas (simbols): %c\n",modified_birth_year);
+printf("modified_birth_year Mainīga vertiba uzreiz pec piešķiršanas (dec): %d\n",modified_birth_year);
+printf("modified_birth_year Mainīga vertiba uzreiz pec piešķiršanas (hex): %x\n",modified_birth_year);
+printf("modified_birth_year Mainīga vertiba uzreiz pec piešķiršanas (oct): %o\n",modified_birth_year);
+
+char c = 10;
+int i = -4569;
+float f = 3.e-16;
+double d = 4.789e39;
+
+printf("c=%d (piešķirtas atmiņas izmērs baitos - %ld,n,\n\t\t",c, sizeof(c));
+printf("atrašanās vieta atmiņā -%p)\n",&c);
+printf("i=%d (piešķirtas atmiņas izmērs baitos - %ld,\n\t\t",i, sizeof(i));
+printf("atrašanās vieta atmiņā -%p)\n",&i);
+printf("f=%.2e (piešķirtas atmiņas izmērs baitos - %ld,\n\t\t",f, sizeof(f));
+printf("atrašanās vieta atmiņā -%p)\n",&f);
+printf("d=%3e (piešķirtas atmiņas izmērs baitos -%ld,\n\t\t",d, sizeof(d));
+printf("atrašanās vieta atmiņā -%p)\n",&d);
+
+
+int *i_adrese = &i;
+printf("i mainīgā adrese - %p\n",i_adrese);
+int *i_adreses_kopija = i_adrese;
+printf("i adreses kopija -%p\n", i_adreses_kopija);
+printf("i mainīgā vērtība, izgūta pielietojot tā adresi - %d\n",*i_adrese);
+printf("i mainīgā vērtība, izgīta pielietojot tā adreses kopju - %d\n",*i_adreses_kopija);
+
+int **i_adreses_adrese=&i_adrese;
+printf("i adreses adrese - %p\n",i_adreses_adrese);
+printf("i adreses adrese izmērs baitos - %ld\n",sizeof(i_adreses_adrese));
 
 return 0;
 }
